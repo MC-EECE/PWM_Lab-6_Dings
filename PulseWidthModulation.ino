e789d1d93b3fa981abe1f20723eefc1b61a67e47
ecotellessa <31664456+ecotellessa@users.noreply.github.com>
2017-11-08 16:19:50 -0500

    Final PWM code

---
+++ PulseWidthModulation.ino
@@ -0,0 +1,27 @@
+const int led_pin=13;
+const int input = A0;
+const int MAX_8_INT = 255;
+int brightness = 0;
+int fade_amount = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  analogReadResolution(12);
+  analogWriteResolution(8);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int PWM_input = analogRead(input)>>4;
+analogWrite(led_pin, PWM_input);
+
+//brightness = brightness + fade_amount;
+//if (brightness == 0 or brightness == MAX_8_INT)
+//{
+//  fade_amount = -fade_amount;
+//}
+//delay(100);
+
+}
